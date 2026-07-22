84c5a611b439aea30c2d375bc826499c7b74cd86
Gaurav Maurya <150830118+Gaurav05180@users.noreply.github.com>
2026-07-22 21:58:06 +0530

    Add brute force solution skeleton

---
+++ Solution1_BruteForce.java
@@ -0,0 +1,16 @@
+public class Solution1_BruteForce {
+
+    public static int longestValidParentheses(String input) {
+
+        // TODO: Implement
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(longestValidParentheses("(()"));
+        System.out.println(longestValidParentheses(")()())"));
+        System.out.println(longestValidParentheses(""));
+    }
+}
